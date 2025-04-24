98fc5fa056da2774c609ae9e2b6b064509afc3cd
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-04-24 11:16:08 +0300

    add spring boot lecture

---
+++ L6-spring-boot/src/main/java/com/lss/l6springboot/L6SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l6springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L6SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L6SpringBootApplication.class, args);
+    }
+
+}
---
+++ L6-spring-boot/src/test/java/com/lss/l6springboot/L6SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.l6springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L6SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
