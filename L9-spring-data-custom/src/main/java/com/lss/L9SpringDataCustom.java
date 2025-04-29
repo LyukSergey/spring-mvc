932a682fb1bbe19ff7d40ad2bb4374f993892a94
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-04-29 16:11:58 +0300

    add spring data custom

---
+++ L9-spring-data-custom/src/main/java/com/lss/L9SpringDataCustom.java
@@ -0,0 +1,13 @@
+package com.lss;
+
+/**
+ * Hello world!
+ *
+ */
+public class L9SpringDataCustom
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
