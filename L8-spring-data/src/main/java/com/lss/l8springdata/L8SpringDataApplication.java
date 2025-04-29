88dea91c5932009a48636383c2ee9ba3ae5750fd
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-04-29 08:48:59 +0300

    add spring data

---
+++ L8-spring-data/src/main/java/com/lss/l8springdata/L8SpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l8springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L8SpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L8SpringDataApplication.class, args);
+    }
+
+}
---
+++ L8-spring-data/src/test/java/com/lss/l8springdata/L8SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.l8springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L8SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
