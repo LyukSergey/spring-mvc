115dc6a36237ce3d2681fb1c66596f564fbf3fab
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-05-01 09:12:43 +0300

    add spring boot spring data custom

---
+++ L9-spring-data-spring-boot-custom/src/main/java/com/lss/l9springdataspringbootcustom/L9SpringDataSpringBootCustomApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l9springdataspringbootcustom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L9SpringDataSpringBootCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L9SpringDataSpringBootCustomApplication.class, args);
+    }
+
+}
---
+++ L9-spring-data-spring-boot-custom/src/test/java/com/lss/l9springdataspringbootcustom/L9SpringDataSpringBootCustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.l9springdataspringbootcustom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L9SpringDataSpringBootCustomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
