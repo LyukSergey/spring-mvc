543ffc39a879eb4d1819a0248a285e7f676d6848
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-05-02 16:15:06 +0300

    add spring security

---
+++ L10-spring-security/src/main/java/com/lss/l10springsecurity/L10SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l10springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L10SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L10SpringSecurityApplication.class, args);
+	}
+
+}
---
+++ L10-spring-security/src/test/java/com/lss/l10springsecurity/L10SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.l10springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L10SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
