cb47173b584b07e292cce13fb1697c6f5e58448b
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-05-05 09:49:43 +0300

    add spring security

---
+++ L10-spring-security/src/main/java/com/lss/l10springsecurity/dbSetup/DatabaseIdAdjuster.java
@@ -0,0 +1,28 @@
+package com.lss.l10springsecurity.dbSetup;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseIdAdjuster implements ApplicationRunner {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public DatabaseIdAdjuster(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Long usersMaxId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM users", Long.class);
+        Long postsMaxId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM posts", Long.class);
+        if (usersMaxId != null) {
+            jdbcTemplate.execute("ALTER TABLE users ALTER COLUMN id RESTART WITH " + (usersMaxId + 1));
+        }
+        if (postsMaxId != null) {
+            jdbcTemplate.execute("ALTER TABLE posts ALTER COLUMN id RESTART WITH " + (postsMaxId + 1));
+        }
+    }
+}
