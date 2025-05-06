b3b679456114abeb8efdceba057c729141e7760a
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-05-06 13:51:01 +0300

    add spring security admin realm client

---
+++ L10-spring-security-admin/src/main/java/com/lss/l10springsecurityadmin/L10SpringSecurityAdminApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l10springsecurityadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L10SpringSecurityAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L10SpringSecurityAdminApplication.class, args);
+    }
+
+}
---
+++ L10-spring-security-admin/src/test/java/com/lss/l10springsecurityadmin/L10SpringSecurityAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.l10springsecurityadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L10SpringSecurityAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
