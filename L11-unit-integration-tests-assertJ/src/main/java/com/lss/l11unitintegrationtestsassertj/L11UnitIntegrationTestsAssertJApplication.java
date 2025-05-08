e267e6555f805b007ae845661e5c0594871ee200
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-05-08 14:53:46 +0300

    add unit and integration tests

---
+++ L11-unit-integration-tests-assertJ/src/main/java/com/lss/l11unitintegrationtestsassertj/L11UnitIntegrationTestsAssertJApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l11unitintegrationtestsassertj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L11UnitIntegrationTestsAssertJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L11UnitIntegrationTestsAssertJApplication.class, args);
+    }
+
+}
---
+++ L11-unit-integration-tests-assertJ/src/test/java/com/lss/l11unitintegrationtestsassertj/L11UnitIntegrationTestsAssertJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.l11unitintegrationtestsassertj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L11UnitIntegrationTestsAssertJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
