53502ce865d198005a31fedcc729e6c85fd9199a
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-05-09 12:41:37 +0300

    add spring security login form DB

---
+++ L10-1-spring-security-DB/src/main/java/com/lss/l101springsecuritydb/L101SpringSecurityDbApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l101springsecuritydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L101SpringSecurityDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L101SpringSecurityDbApplication.class, args);
+    }
+
+}
---
+++ L10-1-spring-security-DB/src/test/java/com/lss/l101springsecuritydb/L101SpringSecurityDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.l101springsecuritydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L101SpringSecurityDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
