f0668583cea5dc4a47b349747fbfd4a98e012ee8
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-05-19 08:55:56 +0300

    add spring security keycloak

---
+++ L10-2-sring-security-keycloak/src/main/java/com/lss/l102sringsecuritykeycloak/L102SpringSecurityKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l102sringsecuritykeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L102SpringSecurityKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L102SpringSecurityKeycloakApplication.class, args);
+	}
+
+}
---
+++ L10-2-sring-security-keycloak/src/test/java/com/lss/l102sringsecuritykeycloak/L102SpringSecurityKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.l102sringsecuritykeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L102SpringSecurityKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
